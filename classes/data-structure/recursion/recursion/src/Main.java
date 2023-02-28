5b94b54a1b81dcfed1ee3a0f95c89970d7715a1b
Marcelo Serra <celo_job@live.com>
2023-02-28 00:26:24 -0300

    feat: add recursive methods

--- classes/data-structure/recursion/recursion/src/Main.java
+++ classes/data-structure/recursion/recursion/src/Main.java
@@ -5,13 +5,26 @@ public class Main {
     public static void main(String[] args) {
 
         Integer recursiveSumMultiplierResult = Recursion.recursiveMultiplierSum(10, 5);
-        System.out.println(recursiveSumMultiplierResult);
+        // System.out.println(recursiveSumMultiplierResult);
 
         Integer recursiveDivisionModuleResult = Recursion.recursiveDivisionModule(11, 5);
-        System.out.println(recursiveDivisionModuleResult);
+        // System.out.println(recursiveDivisionModuleResult);
 
-        Integer[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
-        Integer recursiveArrayEvenNumberCounter = Recursion.recursiveArrayEvenNumberCounter(arr, arr.length - 1, 0);
-        System.out.println(recursiveArrayEvenNumberCounter);
+        Integer[] arr1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+        Integer recursiveArrayEvenNumberCounterResult = Recursion.recursiveArrayEvenNumberCounter(arr1, arr1.length - 1, 0);
+        // System.out.println(recursiveArrayEvenNumberCounterResult);
+
+        Integer recursiveFactorialResult = Recursion.recursiveFactorial(6);
+        // System.out.println(recursiveFactorialResult);
+
+        Integer[] arr2 = {1, -2, 3, -4, 5, 6, -7, 8, 9, -10, 11, -12};
+        Integer recursiveArrayNegativeNumberCounterResult = Recursion.recursiveArrayNegativeNumberCounter(arr2, arr2.length - 1, 0);
+        // System.out.println(recursiveArrayNegativeNumberCounterResult);
+
+        Integer recursiveNumberDigitsCounterResult = Recursion.recursiveNumberDigitsCounter(4056, 1);
+        // System.out.println(recursiveNumberDigitsCounterResult);
+
+        String recursiveReverseStringResult = Recursion.recursiveReverseString("string", "");
+        // System.out.println(recursiveReverseStringResult);
     }
 }
--- classes/data-structure/recursion/recursion/src/Recursion.java
+++ classes/data-structure/recursion/recursion/src/Recursion.java
@@ -1,15 +1,41 @@
 import java.util.List;
 
 public class Recursion {
-    public static int recursiveMultiplierSum(Integer value, Integer multiplier) {
+    public static Integer recursiveMultiplierSum(Integer value, Integer multiplier) {
         return multiplier == 1 ? value : value + recursiveMultiplierSum(value, multiplier - 1);
     }
-    public static int recursiveDivisionModule(Integer value, Integer divisor) {
+    public static Integer recursiveDivisionModule(Integer value, Integer divisor) {
         return value - divisor < 0 ? value : recursiveDivisionModule(value - divisor, divisor);
     }
 
-    public static int recursiveArrayEvenNumberCounter(Integer[] arr, Integer length, Integer evenCount){
+    public static Integer recursiveArrayEvenNumberCounter(Integer[] arr, Integer length, Integer evenCount){
         if(arr[length] % 2 == 0) evenCount++;
         return length == 1 ? evenCount : recursiveArrayEvenNumberCounter(arr, length - 1, evenCount);
+    }
+
+    public static Integer recursiveFactorial (Integer value) {
+        // Conditional: Arg value lesser than 2
+        // Return must be the function with the value minus 1 plus the function with the value minus 2
+        return value < 2 ? value : recursiveFactorial(value - 1) + recursiveFactorial(value - 2);
+    }
+
+    public static Integer recursiveArrayNegativeNumberCounter(Integer[] arr, Integer length, Integer negativeCount){
+        // Conditional: Array length equals 1
+        // Return must be calling the function with the array, array's length minus 1 and the negativeCounter
+        if(arr[length] < 0) negativeCount++;
+        return length == 1 ? negativeCount : recursiveArrayNegativeNumberCounter(arr, length - 1, negativeCount);
+    }
+
+    public static Integer recursiveNumberDigitsCounter(Integer value, Integer counter) {
+        // Conditional: Value divided by 10 lesser than 1
+        // Return must be calling the function with value divided by 10 and counter plus 1
+        return value / 10 < 1 ? counter : recursiveNumberDigitsCounter(value / 10, ++counter);
+    }
+
+    public static String recursiveReverseString(String arg, String reversedArg) {
+        // Conditional: Arg length value equals 0
+        // Return must be calling the function with arg being the string starting from the second position
+        // and reverseArg concatenated with the first string position at left side
+        return arg.length() == 0 ? reversedArg : recursiveReverseString(arg.substring(1), arg.charAt(0) + reversedArg);
     }
 }
