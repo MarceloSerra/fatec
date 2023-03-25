8aeb4b31ee330abb931bedb8192e93ebec7fe23b
Marcelo Serra <celo_job@live.com>
2023-03-25 00:43:09 -0300

    feat: add custom dynamic class and docs

---
+++ classes/data-structure/stack/README.md
@@ -0,0 +1,7 @@
+## Stack 
+
+# Ex 1
+
+## 1.1 ![ex1-1](assets/ex1-1.png)
+
+## 1.2 ![ex1-1](assets/ex1-2.png)
---
+++ classes/data-structure/stack/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        CustomDynamicStack<Integer> stack = new CustomDynamicStack<>();
+
+        Runnable stackTest = () -> {
+            List<Integer> dataList = List.of(new Integer[]{10, 11, 12, 13, 14, 15});
+
+            for(Integer data:dataList) stack.push(data);
+
+            for(Integer i = stack.size(); i > -1; i--) stack.pop(); // Should throw an error
+        };
+        stackTest.run();
+    }
+
+}
